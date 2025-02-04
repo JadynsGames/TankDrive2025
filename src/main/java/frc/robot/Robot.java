ee0380311477841106ea1384e9a9ea187b517c07
JadynsGames <jadynsgamesofficial@gmail.com>
2025-02-04 16:28:47 -0500

    2/4/25

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,17 +12,29 @@ import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.Dumpster;
+import edu.wpi.first.wpilibj.PS5Controller;
+import edu.wpi.first.wpilibj.XboxController;
 
 public class Robot extends TimedRobot {
   SparkMax leftLeader;
   SparkMax leftFollower;
   SparkMax rightLeader;
   SparkMax rightFollower;
-  XboxController joystick;
-
+ // XboxController joystick;
+  PS5Controller joystick;
+  
+  //private RobotContainer m_robotContainer;
+ // class
+  public static final Dumpster dumpster = new Dumpster();
   public Robot() {
+    // Instantiate our RobotContainer. This will perform all our button bindings,
+    // and put our
+    // autonomous chooser on the dashboard.
+    //m_robotContainer = new RobotContainer();
+
     // Initialize the SPARKs
     leftLeader = new SparkMax(25, MotorType.kBrushed);
     leftFollower = new SparkMax(17, MotorType.kBrushed);
@@ -77,7 +89,9 @@ public class Robot extends TimedRobot {
     rightFollower.configure(rightFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
     // Initialize joystick
-    joystick = new XboxController(0);
+    //joystick = new XboxController(0);
+    joystick = new PS5Controller(0);
+
   }
 
   @Override
@@ -85,6 +99,8 @@ public class Robot extends TimedRobot {
     // Display the applied output of the left and right side onto the dashboard
     SmartDashboard.putNumber("Left Out", leftLeader.getAppliedOutput());
     SmartDashboard.putNumber("Right Out", rightLeader.getAppliedOutput());
+
+    CommandScheduler.getInstance().run();
   }
 
   @Override
@@ -105,27 +121,36 @@ public class Robot extends TimedRobot {
      * Get forward and rotation values from the joystick. Invert the joystick's
      * Y value because its forward direction is negative.
      */
-    double maxSpeed = 0.5;
-    double maxRotation = 0.2;
+    double maxSpeed = 0.8;
+    double maxRotation = 0.5;
     // ARCADE
-    //double forward = joystick.getLeftY()*maxSpeed;
-    //double rotation = -joystick.getRightX()*maxRotation;
+    double forward = joystick.getLeftY()*maxSpeed;
+    double rotation = -joystick.getRightX()*maxRotation;
+    
 
     // DIFFERENTIAL
-    double left = joystick.getLeftY()*maxSpeed;
-    double right = joystick.getRightY()*maxSpeed;
+    // double left = joystick.getLeftY()*maxSpeed;
+    // double right = joystick.getRightY()*maxSpeed;
 
     /*
      * Apply values to left and right side. We will only need to set the leaders
      * since the other motors are in follower mode.
      */
     // ARCADE
-    //leftLeader.set(forward + rotation);
-    //rightLeader.set(forward - rotation);
+    leftLeader.set(forward + rotation);
+    rightLeader.set(forward - rotation);
 
     // DIFFERENTIAL
-    leftLeader.set(left);
-    rightLeader.set(right);
+    // leftLeader.set(left);
+    // rightLeader.set(right);
+
+    // DUMPSTER
+    if(joystick.getR2ButtonPressed()){
+      dumpster.activateCoral(0.6);
+    }else if(joystick.getR2ButtonReleased()){
+      dumpster.activateCoral(0);
+    }
+    
   }
 
   @Override
